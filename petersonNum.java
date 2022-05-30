51201a2ab32cacdc66259985a137f3091ecd88bc
Rajeswari <raje03.connect@gmail.com>
2022-05-30 17:15:55 +0530

    Updated while loop

--- petersonNum.java
+++ petersonNum.java
@@ -3,11 +3,12 @@ package numberProject;
 public class petersonNum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Peterson Number
+		
 		int num=148;
 		int temp1,temp2,temp3=0,fact=1;
 		temp1=num;
-		while(num!=0)
+	do
 		{
 			temp2=num%10;
 			for(int i=1;i<=temp2;i++)
@@ -17,7 +18,7 @@ public class petersonNum {
 			temp3=temp3+fact;
 			fact=1;
 			num=num/10;
-		}
+		}	while(num!=0)
 if(temp3==temp1)
 	System.out.println("The Number  "+temp1+"is a Peterson Number");
 else
